44f8e4d5e0d3bea1712747378645b2b06d81b4fd
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2024-02-10 23:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let outPrint = document.getElementById("output");
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+	let inputText = document.getElementById("text");
+	let delay = document.getElementById("delay");
+	printAfterDelay(inputText.value,delay.value).then((res) => console.log(res));
+})
+
+async function printAfterDelay(input, delay){
+	return await new Promise((req,res) => {
+		res(setTimeout(() => {
+			outPrint.innerText = `${input}`
+		},delay))
+	})
+}
